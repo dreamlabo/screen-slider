7e6d3d4896e67266f906d06f4db7b9e36ea410a9
dreamlabo <toddlabo@gmail.com>
2024-02-05 22:24:01 -0700

    Initial Commit

---
+++ scripts.js
@@ -0,0 +1,23 @@
+const introScreen = document.getElementById("intro-screen")
+const topLetters = document.querySelectorAll(".top-logo-letters");
+const logoBottom = document.querySelector(".logo-bottom")
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    setTimeout(() => {
+            topLetters.forEach((letter, index) => {
+                setTimeout(() => {
+                letter.classList.add("active");
+            }, (index + 1) * 300)
+        })
+
+        setTimeout(() => {
+            logoBottom.classList.add("animate-up")
+        }, 1550)
+
+        setTimeout(()=> {
+            introScreen.style.top = '-100vh'
+        }, 2000)
+
+    }, 1500)
+})
